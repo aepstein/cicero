9929ebe2e19a8f009edcb27d8652e7d53bb65328
Ari Epstein <ate2@cornell.edu>
2011-01-04 11:16:23 -0500

    Remove deprecated relative_url_root call.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,5 @@ Cicero::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.action_mailer.delivery_method = :sendmail
-  config.action_controller.relative_url_root='/cicero'
 end
 
