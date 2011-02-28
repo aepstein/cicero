3b75da6c63d5de239a15bd9507aa4d2590c0b0f0
Ari Epstein <ate2@cornell.edu>
2011-02-28 12:00:04 -0500

    Try to correct message addressing.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def ballot_verification( ballot )
     @ballot = ballot
     mail(
-      :to => "#{ballot.user.name} <#{ballot.user.email}>",
+      :to => ballot.user.email,
       :from => ballot.election.contact_email,
       :subject => "Confirmation of #{ballot.election.name} ballot"
     )
