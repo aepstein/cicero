1157f6be2bb8f3ac50f632949c23623b5628fbb1
Ari Epstein <aepstein607@gmail.com>
2013-09-06 14:53:12 -0400

    Re-enable exception notification.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,9 @@ Cicero::Application.configure do
   config.assets.precompile += %w( ie6.css ie7.css print.css )
 #  config.force_ssl = true
   config.filter_parameters += [ :password, :password_confirmation, :ballot ]
-#  config.middleware.use ExceptionNotifier,
-#    :email_prefix => "[cicero] ",
-#    :sender_address => %{"Assemblies IT Support" <assembly-it@cornell.edu>},
-#    :exception_recipients => %w{assembly-it@cornell.edu}
+  config.middleware.use ExceptionNotifier,
+    :email_prefix => "[cicero] ",
+    :sender_address => %{"Assemblies IT Support" <assembly-it@cornell.edu>},
+    :exception_recipients => %w{assembly-it@cornell.edu}
 end
 
