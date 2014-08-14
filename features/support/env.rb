8d4d15421df86217f277118dc10826a830d480af
Ari Epstein <aepstein607@gmail.com>
2014-08-14 09:52:28 -0400

    Remove references to Spork.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,19 +1,14 @@
 require 'rubygems'
-require 'spork'
 
-Spork.prefork do
-  require 'cucumber/rails'
-  Capybara.default_selector = :css
-end
+require 'cucumber/rails'
+Capybara.default_selector = :css
 
-Spork.each_run do
-  ActionController::Base.allow_rescue = false
-  begin
-    require 'database_cleaner'
-    require 'database_cleaner/cucumber'
-    DatabaseCleaner.strategy = :truncation
-  rescue NameError
-    raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
-  end
+ActionController::Base.allow_rescue = false
+begin
+  require 'database_cleaner'
+  require 'database_cleaner/cucumber'
+  DatabaseCleaner.strategy = :truncation
+rescue NameError
+  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
--- features/support/fieldsets.rb
+++ features/support/fieldsets.rb
@@ -1,19 +1,17 @@
-Spork.each_run do
-  module Fieldsets
-    def have_fieldset(handle)
-      have_selector :fieldset, handle
-    end
-    def have_no_fieldset(handle)
-      have_no_selector :fieldset, handle
-    end
-    def has_fielset?(handle)
-      has_selector? :fieldset, handle
-    end
-    def has_no_fieldset?(handle)
-      has_no_selector? :fieldset, handle
-    end
+module Fieldsets
+  def have_fieldset(handle)
+    have_selector :fieldset, handle
   end
-
-  World(Fieldsets)
+  def have_no_fieldset(handle)
+    have_no_selector :fieldset, handle
+  end
+  def has_fielset?(handle)
+    has_selector? :fieldset, handle
+  end
+  def has_no_fieldset?(handle)
+    has_no_selector? :fieldset, handle
+  end
 end
+
+World(Fieldsets)
 
